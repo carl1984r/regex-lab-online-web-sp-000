76711ab3a6d08c7d243a7848ceca2512682b38ed
Bry Cox <carl.cc@gmail.com>
2019-01-23 21:41:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,5 +18,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- var_b = phone.match(/[(\s-]?(\d{3})[)\s-]?\d{3}[\s-]?\d{4}/)
+ phone.match(/[(\s-]?(\d{3})[)\s-]?\d{3}[\s-]?\d{4}/)
 end
